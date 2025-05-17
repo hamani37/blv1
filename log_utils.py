6e4caf863964ba5810eadcc406c113361e5b03ce
hamani37 <hamani376@gmail.com>
2025-05-18 01:08:01 +0200

    log_utils.py

--- log_utils.py
+++ log_utils.py
@@ -1,6 +1,25 @@
-from colorama import Fore, Style, init
-init()
+import json
+from collections import Counter
 
-def log_message(msg):
-    print(Fore.CYAN + msg + Style.RESET_ALL)
+def log_signal(signal_data):
+    try:
+        with open("signals_log.json", "r") as f:
+            logs = json.load(f)
+    except:
+        logs = []
 
+    logs.append(signal_data)
+    with open("signals_log.json", "w") as f:
+        json.dump(logs, f, indent=2)
+
+def print_stats():
+    try:
+        with open("signals_log.json", "r") as f:
+            logs = json.load(f)
+
+        count = Counter([s["signal"] for s in logs])
+        total = len(logs)
+        print(f"📊 Stats (total {total} signaux) : {dict(count)}")
+
+    except:
+        print("Aucune stat disponible")
