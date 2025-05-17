52785eb9bfceffcd02fbb2885482f2b8268384c8
hamani37 <hamani376@gmail.com>
2025-05-18 01:40:35 +0200

    log_utils.py

--- log_utils.py
+++ log_utils.py
@@ -1,9 +1,30 @@
-from datetime import datetime
+import json
+from colorama import Fore, Style
+import os
 
-def log_message(msg):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    line = f"[{timestamp}] {msg}"
-    print(line)
+def log_with_color(signal, price, decision, explanation):
+    print(f"{Fore.YELLOW}[SIGNAL]{Style.RESET_ALL} {signal} | {Fore.GREEN}Prix:{price}{Style.RESET_ALL} | "
+          f"{Fore.CYAN}Décision IA:{decision}{Style.RESET_ALL} | {Fore.MAGENTA}{explanation}{Style.RESET_ALL}")
 
-    with open("logs.txt", "a") as f:
-        f.write(line + "\n")
+def save_signal_data(signal, price, decision, explanation, timestamp):
+    entry = {
+        "timestamp": timestamp,
+        "signal": signal,
+        "price": price,
+        "decision": decision,
+        "explanation": explanation
+    }
+
+    if os.path.exists("live_data.json"):
+        with open("live_data.json", "r") as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                data = []
+    else:
+        data = []
+
+    data.append(entry)
+
+    with open("live_data.json", "w") as file:
+        json.dump(data, file, indent=4)
